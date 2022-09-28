ce2aca8413e462184f359a147a47d5f755290d53
GimmeDaLute <peter.m.edwards@outlook.com>
2022-09-28 16:00:34 +0100

    Edit to data.js for breed/:breed function

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
 const dogBreeds = [
-    { id: 1, breed: "Labrador Retrievers"},
-    { id: 2, breed: "French Bulldogs"},
-    { id: 3, breed: "Golden Retrievers"},
-    { id: 4, breed: "German Shepherds"},
+    { id: 1, breed: "Labrador-Retrievers"},
+    { id: 2, breed: "French-Bulldogs"},
+    { id: 3, breed: "Golden-Retrievers"},
+    { id: 4, breed: "German-Shepherds"},
     { id: 5, breed: "Poodles"},
     { id: 6, breed: "Bulldogs"},
     { id: 7, breed: "Beagles"},
     { id: 8, breed: "Rottweilers"},
     { id: 9, breed: "Pointers"},
     { id: 10, breed: "Dashunds"},
-    { id: 11, breed: "Pembroke Welsh Corgis"}, // Not seen with range of 10
-    { id: 12, breed: "Australian Shepherds"},
-    { id: 13, breed: "Yorkshire Terriers"},
+    { id: 11, breed: "Pembroke-Welsh-Corgis"}, // Not seen with range of 10
+    { id: 12, breed: "Australian-Shepherds"},
+    { id: 13, breed: "Yorkshire-Terriers"},
     { id: 14, breed: "Boxers"},
-    { id: 15, breed: "King Charles Spaniels"},
-    { id: 16, breed: "Doberman Pinschers"},
-    { id: 17, breed: "Great Danes"},
-    { id: 18, breed: "Miniature Schnauzers"},
-    { id: 19, breed: "Siberian Huskies"},
-    { id: 20, breed: "Bernese Mountain Dogs"},
-    { id: 21, breed: "Cane Corsos"}
+    { id: 15, breed: "King-Charles-Spaniels"},
+    { id: 16, breed: "Doberman-Pinschers"},
+    { id: 17, breed: "Great-Danes"},
+    { id: 18, breed: "Miniature-Schnauzers"},
+    { id: 19, breed: "Siberian-Huskies"},
+    { id: 20, breed: "Bernese-Mountain-Dogs"},
+    { id: 21, breed: "Cane-Corsos"}
 
 ];
 
