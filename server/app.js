cf24b27d49389b862f6e96858344c72eaf047ce4
Brendan Geoghegan <brendangeoghegan@outlook.com>
2022-09-29 16:09:36 +0100

    testing set up

--- server/app.js
+++ server/app.js
@@ -6,9 +6,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors());
 
-const port = 8000;
-
 const dogRoutes = require("./controllers/dogs");
 app.use("/dogs", dogRoutes);
 
-app.listen(port, () => console.log(`Poodle Search app live on http://localhost:${port}!`))
+module.exports =  app;
